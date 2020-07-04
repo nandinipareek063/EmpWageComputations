d34937831ca555122e8850f7d6ab2ce4828721be
nandinipareek063 <nandinipareek063@gmail.com>
2020-07-04 21:06:04 +0530

    Added: Daily Emp Wage func is added

--- empWage.sh
+++ empWage.sh
@@ -1,1 +1,11 @@
-"Welcome to Employee Wage Computation"
+echo "Welcome to Employee Wage Computation"
+#UC1
+Attendence=$(( $RANDOM % 2 + 1 ))
+if [ $Attendence -eq 1 ]
+then
+employ="Present"
+echo $employ
+else
+employ="Absent"
+echo $employ
+fi
